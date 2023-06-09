6de13319d0489052e2357ac0836620c5925648ed
Fatema Moharam <moharamfatema@gmail.com>
2023-06-09 19:45:55 +0300

    * better styles

--- src/app/modules/Nav/NavBar.tsx
+++ src/app/modules/Nav/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
             : appPaths;
 
     return (
-        <nav className='navbar grid grid-flow-col grid-rows-1 grid-cols-4 bg-orange-500 text-white font-bold shadow-md sticky top-0 w-full'>
+        <nav className='navbar grid grid-flow-col grid-rows-1 grid-cols-3 bg-primary text-white font-bold shadow-md sticky top-0 w-full'>
             <div
                 id='logo'
                 className='logo flex flex-row items-center col-span-1 p-3 gap-2 font-extrabold'
@@ -20,9 +20,16 @@ const NavBar = () => {
                     alt='logo'
                     className='p-1 bg-white rounded-2xl'
                 />
-                <h1 className='text-2xl'>Vivit Violence</h1>
+                <div className='logo-text flex flex-row items-center h-full gap-2'>
+                    <h1 className='text-2xl w-min'>Vivit Violence</h1>
+                    <div className='divider h-full w-px bg-white'></div>
+                    <div className='decription text-sm font-normal '>
+                        Video Vision Transformers For Hierarchical Anomaly
+                        Detection
+                    </div>
+                </div>
             </div>
-            <ul className='navbar__elements flex-row items-center justify-evenly col-span-3 text-xl hidden sm:flex'>
+            <ul className='navbar__elements flex-row items-center justify-evenly col-span-2 text-xl hidden sm:flex'>
                 {paths.map(path => (
                     <li className='navbar__element p-3' key={path.path}>
                         <Link to={path.path}>{path.name}</Link>
