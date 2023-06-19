593b8b3f9a2a835043eaf26fe3d7dab6ca340cf4
Nourhan Waleed <nouraaboelsoaoud@yahoo.com>
2023-06-19 22:01:31 +0300

    added my info

--- src/app/assets/data/team.ts
+++ src/app/assets/data/team.ts
@@ -1,8 +1,7 @@
 import { IPerson } from '../../types/person';
 import Fatema from '../../assets/img/team/fatema.png';
-import Khafaga from '../../assets/img/team/Khafaga.png';
-import Raslan from '../../assets/img/team/Raslan.png';
 const placeholder = 'https://dummyimage.com/100x100/fff/333';
+import Nourhan from '../../assets/img/team/nourhan.png';
 
 export const team: IPerson[] = [
     {
@@ -17,30 +16,26 @@ export const team: IPerson[] = [
         kaggle: 'https://kaggle.com/fatemamoharam',
     },
     {
-        name: 'Youssef Khafaga',
-        title: 'SWE | ML Engineer',
-        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel.',
-        picture: Khafaga,
-        github: 'https://github.com/YoussefKhafaga',
-        linkedin: 'https://www.linkedin.com/in/youssef-khafaga-655b18205/',
-        email: 'ykhfaga@gmail.com',
-        kaggle: '',
-    },
-    {
-        name: 'Nourhan Aboelsoaoud',
+        name: 'Yousef Khafaga',
         title: 'SWE | ML Engineer',
         bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel.',
         picture: placeholder,
     },
     {
+        name: 'Nourhan Aboelsoaoud',
+        title: 'SWE | ML Engineer',
+        bio: 'A hardworking machine learning engineer with passion for research and development, and always learning about new technologies.',
+        picture: Nourhan,
+        github: 'https://github.com/NourhanWaleed',
+        linkedin: 'https://www.linkedin.com/in/nourhan-waleed-964230214',
+        email: 'nourhanaboelsoaoud@gmail.com'
+
+    },
+    {
         name: 'Mohamed Raslan',
-        title: 'SWE | ML Engineer | Data Scientist',
-        bio: 'Mohamed is a driven student and a dedicated machine learning enthusiast with an interset in data science coupled with a strong background in data analysis and deep learning algorithms, he played a key role in developing and fine-tuning our violence detection model.',
-        picture: Raslan,
-        github: 'https://github.com/mraslann',
-        linkedin: 'https://www.linkedin.com/in/mohamed-raslan1/',
-        email: 'raslanmohamed.mr@gmail.com',
-        kaggle: 'https://www.kaggle.com/mohamedraslann'
+        title: 'SWE | ML Engineer',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis egestas rhoncus. Donec facilisis fermentum sem, ac viverra ante luctus vel.',
+        picture: placeholder,
     },
     {
         name: 'Mohanad Bashar',
